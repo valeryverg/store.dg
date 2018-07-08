6bad6e8b1b34c44a188f097aaefc4f9e55fbbdb5
Valery <valeryverg@bmail.com>
2018-07-08 05:49:29 +0300

    180807

---
+++ database/migrations/2018_07_07_102126_create_table_menuitems.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableMaterials extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('type');
+            $table->string('size');
+            $table->string('density');
+            $table->string('color');
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
---
+++ resources/views/store/menu.nlade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 08.07.2018
+ * Time: 5:22
+ */
