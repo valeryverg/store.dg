eff24b906f350e726b5b86e6871607c6e21a96d3
valeryverg <38358050+valeryverg@users.noreply.github.com>
2018-07-08 08:43:02 +0300

    180708

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,23 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\MenuItem;
-use Spatie\Menu\Laravel\Menu;
 
 class IndexController extends Controller
 {
 
     public function show()
     {
-        $items = MenuItem::all()->sortBy('sort_by');
-
-        $menus = Menu::build($items, function ($menu, $item) {
-            $menu->action($item->action.'@'.$item->link, $item->name);
-        });
-        $menus->render();
-        //dump($items);
-
-        // return view('store.index')->with('items', $menus);
-        return view('store.index')->with('items', $items);
+        $menu = MenuItem::all()->sortBy('sort_by');
+        return view('store.index', ['menu' => $menu]);
     }
 
     public function admin()
--- resources/views/store/layout/menu.blade.php
+++ resources/views/store/layout/menu.blade.php
@@ -1,29 +1,30 @@
 @extends('store.layout.base')
 
 @section('navbar')
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <a class="icon-bar" href="#"><img src="{{ asset('images/store/logo/logo_dniprograf.png') }}"
-                                              alt="Дніпрограф" height="50" width="160"></a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                @foreach($items as $item)
-                <li>
-                    <a href="/{{ $item->link }}">{{ $item->name }}</a>
-                </li>
-                @endforeach
-            </ul>
-            <form class="navbar-form navbar-right">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Введіть слово">
-                </div>
-                <button type="submit" class="btn btn-default">Пошук...</button>
-            </form>
-        </div>
-    </div><!-- /.container-fluid -->
-</nav>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="icon-bar" href="#"><img src="{{ asset('images/store/logo/logo_dniprograf.png') }}"
+                                                  alt="Дніпрограф" height="50" width="160"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                @if (isset($menu))
+                    <ul class="nav navbar-nav">
+                        @foreach($menu as $item)
+                            <li>
+                                <a href="/{{ $item['link'] }}">{{ $item['name'] }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+                <form class="navbar-form navbar-right">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Введіть слово">
+                    </div>
+                    <button type="submit" class="btn btn-default">Пошук...</button>
+                </form>
+            </div>
+        </div><!-- /.container-fluid -->
+    </nav>
 @endsection
